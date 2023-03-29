fb237402ef2cf43b93f068bf2b6c6b80a10dfd21
minaalbert33 <89715882+minaalbert33@users.noreply.github.com>
2023-03-29 07:14:50 +0200

    Add footer.js

---
+++ js/footer.js
@@ -0,0 +1,5 @@
+let footer = ` <div>
+<span>Copyright © 2004-2015 Faculty of Computers and Artificial Intelligence 5 Dr. Ahmed Zewail Street, Postal Code: 12613, Orman, Giza, Egypt.</span>
+</div>`;
+
+document.getElementById("website-footer").innerHTML = footer;
