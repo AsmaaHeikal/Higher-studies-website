78ccc89b17d3bcdc36d5df5d32323eff6b66388d
minaalbert33 <89715882+minaalbert33@users.noreply.github.com>
2023-04-14 05:21:04 +0200

    Edit header and footer styling & Add about.html, about.css

--- js/footer.js
+++ js/footer.js
@@ -1,5 +1,5 @@
 let footer = ` <div>
-<span>Copyright © 2004-2015 Faculty of Computers and Artificial Intelligence 5 Dr. Ahmed Zewail Street, Postal Code: 12613, Orman, Giza, Egypt.</span>
+<p>Copyright © 2004-2015 Faculty of Computers and Artificial Intelligence 5 Dr. Ahmed Zewail Street, Postal Code: 12613, Orman, Giza, Egypt.</p>
 </div>`;
 
 document.getElementById("website-footer").innerHTML = footer;
