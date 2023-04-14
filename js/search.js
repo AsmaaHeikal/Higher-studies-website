df6c5aa52c1eecdd6a10af53ddc6ec88888ff6de
minaalbert33 <89715882+minaalbert33@users.noreply.github.com>
2023-04-14 18:06:34 +0200

    Delete button from edit icons

--- js/search.js
+++ js/search.js
@@ -1,7 +1,8 @@
-let v = document.getElementsByClassName("edit");
-// alert(v.length);
-v.forEach(function(element) {
-    element.onclick = function(){
+// let v = document.getElementsByClassName("edit");
+// // alert(v.length);
+// v.forEach(function(element) {
+//     element.onclick = function(){
         
-    };
-});
+//     };
+// });
+
