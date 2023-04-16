4c3ac918f85714ed3b4e68d890578df41e1ebc7f
Ahmed Zaher <66391501+AhmedZaher5788@users.noreply.github.com>
2023-04-16 19:11:15 +0200

    Update registerCourses.js

--- js/registerCourses.js
+++ js/registerCourses.js
@@ -1,3 +1,4 @@
+addData();
 function getStudentCourses(){
     let data = localStorage.getItem('students');
     let students = JSON.parse(data);
@@ -35,29 +36,58 @@ function getCourseHours(CourseId){
         let courseMarks = studentCourses[i][courseId].split(',')[0];
         let finalMarks = studentCourses[i][courseId].split(',')[1];
         let totalMarks = (+courseMarks) + (+finalMarks); // unary operator to sum two strings
-        console.log(courseId[0]);
-        console.log(courseName);
-        console.log(courseHours);
-        console.log(courseMarks);
-        console.log(finalMarks);
-        console.log(totalMarks);
         addRow(courseId[0], courseName, courseHours, courseMarks, finalMarks, totalMarks);
     }
  }
- addData();
+ 
 function addRow(courseId, courseName, courseHours, courseMarks, finalMarks, totalMarks) {
 
+    // Get extra data
+    let coursesArray = JSON.parse(localStorage.getItem('courses'));
+    let department, lecture_day, hall_number;
+    for(j = 0; j < coursesArray.length; j++){
+        if(coursesArray[j]['id'] == courseId){
+            department = coursesArray[j]['department'];
+            lecture_day = coursesArray[j]['lecture_day'];
+            hall_number = coursesArray[j]['hall_number'];
+        }
+    }
+
+    //-----------------------------
     let body = document.getElementById('mainTableBody');
 
     body.innerHTML += 
     `
-    <tr>
+    <tr class = "parent">
         <td>${courseId}</td>
         <td>${courseName}</td>
         <td>${courseHours}</td>
         <td>${courseMarks}</td>
         <td>${finalMarks}</td>
         <td>${totalMarks}</td>
+        <td><button class="expand-button" >Expand</button></td>
+        <div class = "expanded-div" , style="display:none;">
+            <p>Department :  ${department}</p>
+            <p> Lecture Day : ${lecture_day}</p>
+            <p>Hall Number : ${hall_number}</p>
+        </div>
     </tr>
+    
     `;
  }
+
+// Select all buttons with class "expand-button"
+var buttons = document.querySelectorAll('.expand-button');
+
+// Loop through all buttons and add event listeners
+buttons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    // Find the next div and toggle its display style property
+    var div = this.parentNode.parentNode.nextSibling;
+    if (div.style.display === 'block') {
+      div.style.display = 'none';
+    } else {
+      div.style.display = 'block';
+    }
+  });
+});
